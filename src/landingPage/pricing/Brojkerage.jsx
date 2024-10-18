616f9c8427bf82292753bdaa7ccaa16852d0836d
Rishi Goyal <rishig516@gmail.com>
2024-10-18 12:59:14 +0530

    Component setup

---
+++ src/landingPage/pricing/Brojkerage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Brojkerage() {
+  return (
+    <div>Brojkerage</div>
+  )
+}
+
+export default Brojkerage
---
+++ src/landingPage/pricing/PricingPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../Navbar";
+import Hero from "./Hero";
+import Brojkerage from "./Brojkerage";
+import Footer from "../Footer";
+
+function PricingPage() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Brojkerage />
+      <Footer />
+    </>
+  );
+}
+
+export default PricingPage;
